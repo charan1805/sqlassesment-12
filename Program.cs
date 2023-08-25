e10430e3618d15c9b04e92a24c42139b302d45bc
charan1805 <sreecharanmudhiraj@gmail.com>
2023-08-25 08:20:23 +0530

    First Class

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET_StoredProcedure
+{
+    internal class Program
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static string conStr = "server = CHARAN-PC; database = OrderDB; trusted_connection = true;";
+        static void Main(string[] args)
+        {
+            try
+            {
+                con = new SqlConnection(conStr);
+                con.Open();
+
+                cmd = new SqlCommand()
+                {
+                    CommandText = "PlaceOrder",
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = con
+                };
+
+                Console.Write("Enter Customer ID : ");
+                cmd.Parameters.AddWithValue("@customerId", int.Parse(Console.ReadLine()));
+                Console.Write("Enter Total Amount : ");
+                cmd.Parameters.AddWithValue("@totalAmount", double.Parse(Console.ReadLine()));
+
+                int output = (int)cmd.ExecuteScalar();
+
+                if (output >= 1)
+                {
+                    Console.WriteLine("Order Placed Successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Order Placing Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
